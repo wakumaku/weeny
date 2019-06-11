135b6463133b0e09712ad6f023570f92aa783200
wakumaku <wakumaku@gmail.com>
2019-06-11 21:12:22 +0200

    adds cache and hasher info on start

--- src/cmd/http/main.go
+++ src/cmd/http/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
-	"github.com/rs/zerolog"
-	"github.com/rs/zerolog/log"
+	"fmt"
 	"weeny/apiserver"
 	"weeny/application"
 	"weeny/config"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
 func main() {
@@ -13,12 +15,17 @@ func main() {
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
 	log.Info().Msg("== weeny ==")
 
+	c := config.CreateCacheFromConfig()
+	h := config.CreateHasherFromConfig()
+
 	s := apiserver.NewServer(
 		application.New(
-			config.CreateCacheFromConfig(),
-			config.CreateHasherFromConfig(),
+			c,
+			h,
 		),
 	)
+
+	log.Info().Str("cache", fmt.Sprintf("%T", c)).Str("hasher", fmt.Sprintf("%T", h)).Msg("Initializing")
 
 	if err := s.Start(8000); err != nil {
 		log.Error().Msgf("Initializing server: %s", err)
