2ec509c0e9a3958ceb32a5cdd2acf6b7fde79714
wakumaku <wakumaku@gmail.com>
2019-06-11 14:18:37 +0200

    integration test

--- src/tests/integration/integration_test.go
+++ src/tests/integration/integration_test.go
@@ -1,9 +1,12 @@
 package integration
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"os"
+	"reflect"
+	"strings"
 	"testing"
 	"time"
 	"weeny/apiserver"
@@ -12,18 +15,28 @@ import (
 	"weeny/hasher"
 )
 
+type response struct {
+	Message string `json:"message"`
+	Data    string `json:"data"`
+}
+
 var s *apiserver.ApiServer
+
+const (
+	serverPort = 4444
+	baseURL    = "http://localhost:4444"
+)
 
 func startServer() {
 
 	s = apiserver.NewServer(
 		application.New(
-			cache.NewRedis("redis", 6739, "", 0),
+			cache.NewRedis("redis", 6379, "", 0),
 			&hasher.Md5{},
 		),
 	)
 
-	if err := s.Start(4444); err != nil {
+	if err := s.Start(serverPort); err != nil {
 		// nice
 	}
 }
@@ -34,7 +47,7 @@ func waitForServer() {
 		if started.After(time.Now().Add(5 * time.Second)) {
 			panic("Time out waiting for server to start")
 		}
-		resp, err := http.Get("http://localhost:4444/ping")
+		resp, err := http.Get(baseURL + "/ping")
 		if err != nil {
 			continue
 		}
@@ -60,23 +73,110 @@ func TestMain(m *testing.M) {
 	os.Exit(r)
 }
 
-func TestPing(t *testing.T) {
-	resp, err := http.Get("http://localhost:4444/ping")
+func TestGetNonExistingHash(t *testing.T) {
+
+	expectedResponse := response{Message: "Failure"}
+
+	resp, err := http.Get(baseURL + "/here_an_unexisting_hash")
 	if err != nil {
 		panic(err)
 	}
 	defer resp.Body.Close()
+
 	body, err := ioutil.ReadAll(resp.Body)
-	if string(body) != "Pong" {
+	if err != nil {
+		t.Fatalf("reading response body: %s", err)
+	}
+
+	var r response
+	if err := json.Unmarshal(body, &r); err != nil {
+		t.Fatalf("unmarshalling response body: %s", err)
+	}
+
+	if !reflect.DeepEqual(expectedResponse, r) {
 		t.Fatalf("%s : %s", body, err)
 	}
 }
 
-// resp, err = http.PostForm("http://duckduckgo.com",
-// 		url.Values{"q": {"github"}})
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	defer resp.Body.Close()
-// 	body, err = ioutil.ReadAll(resp.Body)
-// 	fmt.Println("post:\n", keepLines(string(body), 3))
+func TestPostMalformedURL(t *testing.T) {
+
+	bodyPost := `{"url":"d u ckduckgo.com"}`
+	expectedPostResponse := response{
+		Message: "Not a valid URL",
+		Data:    "",
+	}
+
+	resp, err := http.Post(baseURL+"/shortern", "application/json", strings.NewReader(bodyPost))
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatalf("reading response body: %s", err)
+	}
+
+	var r response
+	if err := json.Unmarshal(body, &r); err != nil {
+		t.Fatalf("unmarshalling response body: %s", err)
+	}
+
+	if !reflect.DeepEqual(expectedPostResponse, r) {
+		t.Fatalf("%s : %s", body, err)
+	}
+}
+func TestPostURLAndLookupHash(t *testing.T) {
+
+	// POST an url
+	bodyPost := `{"url":"http://duckduckgo.com"}`
+	expectedPostResponse := response{
+		Message: "Success",
+		Data:    "d4faf77f2107085ad92c39bc47530014",
+	}
+
+	resp, err := http.Post(baseURL+"/shortern", "application/json", strings.NewReader(bodyPost))
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatalf("reading response body: %s", err)
+	}
+
+	var r response
+	if err := json.Unmarshal(body, &r); err != nil {
+		t.Fatalf("unmarshalling response body: %s", err)
+	}
+
+	if !reflect.DeepEqual(expectedPostResponse, r) {
+		t.Fatalf("%s : %s", body, err)
+	}
+
+	// GET a hash
+	expectedGetResponse := response{
+		Message: "Success",
+		Data:    "http://duckduckgo.com",
+	}
+
+	resp, err = http.Get(baseURL + "/lookup/d4faf77f2107085ad92c39bc47530014")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatalf("reading response bodyx: %s", err)
+	}
+
+	if err := json.Unmarshal(body, &r); err != nil {
+		t.Fatalf("unmarshalling response body: %s", err)
+	}
+
+	if !reflect.DeepEqual(expectedGetResponse, r) {
+		t.Fatalf("%s : %s", body, err)
+	}
+}
